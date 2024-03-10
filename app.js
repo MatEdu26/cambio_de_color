8ddf0fb4f32d86519be994bd12bd18542a5b0231
Matias Bassi <mebassi87@gmail.com>
2024-03-10 07:53:44 -0300

    sugunda funcion y cambio de aspecto

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//funcion cambio de color background
+
 let color_bg = document.getElementById('color_bg');
 
 let color_body = document.getElementById('cuerpo')
@@ -9,3 +11,15 @@ function cambio_back_color() {
 
 color_bg.addEventListener('input', cambio_back_color);
 
+//funcion cambio de color titulo
+
+let color_titulo = document.getElementById('color_titulo');
+
+let title = document.getElementById('titulo');
+
+function color_title() {
+    let color = color_titulo.value;
+    title.style.color = color;
+}
+
+color_titulo.addEventListener('input', color_title);
